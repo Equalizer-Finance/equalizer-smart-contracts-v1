357ba25164120cc4ee2bf10234394ac26a5cf8ab
Stefan Bancu <Stefan>
2022-07-27 09:12:31 +0300

    fix: sandwitch potential attack

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -96,6 +96,16 @@ contract Vault is
     }
 
     /**
+     * @dev Getter get an output amount for exact input.
+     * @return number of LP tokens for an exact input
+     */
+
+    function getAmountOutputForExactInput(uint256 input) external view virtual override returns (uint256 receivedETokens) {
+        require(amount > 0, 'CANNOT_STAKE_ZERO_TOKENS');
+        receivedETokens = getNrOfETokensToMint(amount);
+    }
+
+    /**
      * @dev Setter for max capacity.
      * @param _maxCapacity new value to be set.
      */
@@ -123,7 +133,7 @@ contract Vault is
      * @dev Provide liquidity to Vault.
      * @param amount The amount of liquidity to be deposited.
      */
-    function provideLiquidity(uint256 amount) external onlyNotPaused nonReentrant {
+    function provideLiquidity(uint256 amount, uint256 minOutputAmount) external onlyNotPaused nonReentrant {
         require(amount > 0, 'CANNOT_STAKE_ZERO_TOKENS');
         require(amount + totalAmountDeposited <= maxCapacity, 'AMOUNT_IS_BIGGER_THAN_CAPACITY');
 
